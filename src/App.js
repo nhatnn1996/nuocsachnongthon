58ebc0e9f898a4d0526d448da957f3dfa4150064
Nhatnn <nhatnn@galaxyplay.vn>
2020-10-18 05:02:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from "react";
+import Search from "./components/search";
+import WarterBill from "./components/water-bill";
+import "antd/dist/antd.css";
+import "./App.css";
+
+export const WarterBillCtx = createContext();
 
 function App() {
+  const [store, setStore] = useState({});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WarterBillCtx.Provider value={{ store, setStore }}>
+        <header className="App-header">Ứng dụng tra cứu tiền nước</header>
+        <main className="Main">
+          <Search />
+          <WarterBill />
+        </main>
+      </WarterBillCtx.Provider>
     </div>
   );
 }
